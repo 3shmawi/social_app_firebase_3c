c18f198cb54b8aab13c8cd30dc2fabca0049fb40
ashmawi <mohamedashmawy918@gmail.com>
2024-08-05 21:18:29 +0300

    new story

--- lib/screens/_resources/assets_path/assets_manager.dart
+++
@@ -1,8 +0,0 @@
-abstract class AssetsManager {
-  static const _imgPath = "assets/images";
-  static const _iconPath = "assets/icons";
-
-  static const logo = "$_imgPath/logo.png";
-}
-
-//clean code
